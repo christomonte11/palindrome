8d645ebe442190db8c6c820ce8a89e84334ccfe9
Uwe Michael Scheffer <uwe.scheffer@scheffer-software.de>
2018-11-01 12:34:44 +0100

    Initialize repository

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+    return Array.from(this).reverse().join('');
+};
+
+// Defines a Phrase object.
+function Phrase(content) {
+    this.content = content;
+
+    // Returns content processed for palindrome testing.
+    this.processedContent = function processedContent() {
+        return this.content.toLowerCase();
+    };
+
+    // Returns true if the phrase is a palindrome, false otherwise.
+    this.palindrome = function palindrome() {
+        let processedContent = this.processedContent();
+        return processedContent === processedContent.reverse();
+    };
+}
+
+// $ node
+// > .load palindrome.js
+// > let napoleonsLament = new Phrase("Able was I ere I saw Elba");
+// undefined
+// > napoleonsLament.palindrome();
+// true
+
